8fdd2741c48cfb9f420a058489e6e560b0b62b48
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-08-19 23:01:28 +0530

    updated user controller
    updated user controller

--- api/user.controller.js
+++ api/user.controller.js
@@ -1,22 +1,6 @@
 const mongoose = require("mongoose");
 const { failedResponse, successResponse,ObjectId } = require("../util/response.helper");
-// const connectDatabase = async () => {
-//   try {
 
-
-    
-//     await mongoose.connect(process.env.MONGODBURL || "mongodb://localhost:27017",{
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true,
-//         });
-
-//     console.log("connected to database");
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// };
-// connectDatabase();
 
 let Schema = mongoose.Schema;
 
@@ -50,13 +34,24 @@ let UserSchema = new Schema(
   }
 );
 
-// UserSchema.methods.toJSON = function () {
-//   var user = this;
-//   var userObject = user.toObject();
-//   return userObject;
-// };
-
 var User = mongoose.model("users", UserSchema);
+
+
+
+const getUser = async (req, res) => {
+  let { id } = req.params;
+  try {
+    const data = await User.findOne({ email: id })
+    .then((res) => res.toJSON());
+    if (!data) {
+        
+      throw new Error("A user with this email could not be found.");
+    }
+    return successResponse(res, data);
+  } catch (e) {
+    return failedResponse(res, e.toString());
+  }
+};
 
 const getAllUsers = async (req, res) => {
   try {
@@ -70,9 +65,67 @@ const getAllUsers = async (req, res) => {
     return failedResponse(res, e.toString());
   }
 };
-const UserController = {
-  getAllUsers
+
+const getUsersById = async (req, res) => {
+  try {
+    const data = await User.find({ $and: [{ email: req.body.emails }] })
+      .select("-password")
+      .select("address name email phone username")
+      .then((res) => res.toJSON());
+    return successResponse(res, data);
+  } catch (e) {
+    return failedResponse(res, e.toString());
+  }
 };
 
+const updateUserDetail = async (req, res) => {
+  let { body } = req;
+  let { email } = body;
+  try {
+    let data = await User.findOne({ email });
+    if (!data) {
+      throw new Error("Email id is not available");
+    } else {
+      await User.create(body);
+      let query = { email };
+      let updates = { ...body };
+      let doc = await User.updateOne(query, updates);
+      return successResponse(res, doc);
+    }
+  } catch (e) {
+    return failedResponse(res, e.toString());
+  }
+};
+
+const deleteUserDetail = (req, res) => {
+  return res.json({
+    status: RESPONSE_STATUS.SUCCESS,
+    message: "Delete By User Id",
+  });
+};
+const createUserDetail = async (req, res, next) => {
+  let { body } = req;
+  try {
+    let data = await User.findOne({ email: body.email });
+    if (data) {
+      throw new Error("User detail already available");
+    } else {
+      await User.create(body);
+      let temp = User.findOne({ email: body.email }).then((res) => res);
+      return successResponse(res, temp);
+    }
+  } catch (e) {
+    return failedResponse(res, e.toString());
+  }
+};
+
+const UserController = {
+  getAllUsers,
+  getUser,
+  getUsersById,
+  updateUserDetail,
+  deleteUserDetail,
+  createUserDetail,
+};
 module.exports = {UserController,User};
 
--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const Controller  = require("../api/user.controller");
 
 /* GET user operation. */
-// router.get("/:id", (req, res, next) => UserController.getUser(req, res));
-console.log(router)
+router.get("/:id", (req, res, next) => UserController.getUser(req, res));
+
 router.get("/get/all", (req, res, next) => Controller.UserController.getAllUsers(req,res));
 
 
-// /* POST user operation. */
-// router.post("/new", (req, res, next) => UserController.createUserDetail(req, res, next));
-// router.put("/update", (req, res, next) => UserController.updateUserDetail(req, res));
-// router.post("/queryByIds",(req, res, next) => UserController.getUsersById(req, res));
+/* POST user operation. */
+router.post("/new", (req, res, next) => UserController.createUserDetail(req, res, next));
+router.put("/update", (req, res, next) => UserController.updateUserDetail(req, res));
+router.post("/queryByIds",(req, res, next) => UserController.getUsersById(req, res));
 
 
 module.exports = router;
